2eb38187f68158205e3722862d5b8abe639b0f2b
Shaik Afrid <shaikafrid709@gmail.com>
2025-09-09 20:32:22 +0530

    Prepare for deployment: add start script and configurable API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/todos",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/todos",
 })
 
 export default API;
